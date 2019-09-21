bad47faf076edcc5fb1b94cf6438ec859276c8cb
Debora Escudeiro <51325568+deboraesc@users.noreply.github.com>
2019-09-21 17:16:46 -0300

    Add files via upload

---
+++ precache-manifest.5b26c08144610ad66b29c693ad9081c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82400e81846b49c94b8c415e6cd4aaeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99de391b1512aa521b51",
+    "url": "/static/css/main.10eb2faa.chunk.css"
+  },
+  {
+    "revision": "61c8c26cbecc4d327b28",
+    "url": "/static/js/2.8d6ee4ec.chunk.js"
+  },
+  {
+    "revision": "99de391b1512aa521b51",
+    "url": "/static/js/main.d8d66ca6.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "7160d42ce68c7c54cb00a85940907c3a",
+    "url": "/static/media/eu.7160d42c.jpg"
+  }
+]);
